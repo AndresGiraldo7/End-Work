2648ac6fa15c12239366cbded65b673724cbf44b
AndresggDev <andgg.dev@gmail.com>
2022-08-24 16:56:39 -0500

    make it better

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css" type="text/css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+    <br>
+    <h1><strong>Sistema de Alertas Tipo Semaforo</strong></h1><br><br>
+    
+    <table>
+        <thead>
+            <th>Item</th>
+            <th>Contenido</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    1.
+                </td>
+                <td>
+                    Introducción
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    2.
+                </td>
+                <td>
+                    Planteamiento del problema
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    3.
+                </td>
+                <td>
+                    Justificacion del problema
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    4.
+                </td>
+                <td>
+                    Objetivo General
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    5.
+                </td>
+                <td>
+                    Objetivo Especifico
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    6.
+                </td>
+                <td>
+                    Marco Teorico
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    7.
+                </td>
+                <td>
+                    Tecnologias y Lenguajes de Programacion
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    8.
+                </td>
+                <td>
+                    Metodologías para el desarrollo de aplicaciones web
+                </td>
+
+            </tr>
+
+            <tr>
+                <td>
+                    9.<br><br>
+                    9.1 <br><br>
+                    9.2 <br><br>
+                    9.3 <br><br>
+                    9.4 <br><br>
+                    9.5<br><br>
+
+                </td>
+                <td>
+                    <strong>Fases del Proyecto Aplicativo Web</strong><br> <br>
+                    Análisis de Requisitos del Software<br><br>
+                    Diseño del sistema <br><br>
+                    Codificación<br> <br>
+                    Pruebas y Error<br> <br>
+                    Validacíon y Verificacíon del Producto Software <br> <br>
+            </tr>
+
+
+            <tr>
+                <td>
+                    10. <br><br>
+                    10.1<br><br>
+                    10.1.1<br><br>
+                    10.1.2<br><br>
+                    10.1.3<br><br>
+                    10.1.4<br><br>
+                    10.1.5<br><br>
+
+                </td>
+                <td>
+                    <strong>Desarrollo de la Aplicación </strong><br><br>
+                    <strong>Fase de Análisis</strong><br><br>
+                    Diagrama de Clases<br><br>
+                    Diagrama Entidad Relación <br><br>
+                    Diagrama de Casos de Uso<br><br>
+                    Diagrama de Flujo de Datos<br><br>
+                    Diagramas de procesos <br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    11. <br><br>
+                    11.1<br><br>
+                    11.2<br><br>
+                    11.3<br><br>
+                    11.4<br><br>
+
+                </td>
+                <td>
+                    <strong>Fase de Diseño y Desarrollo</strong><br><br>
+                    Módulo Usuarios<br><br>
+                    Módulo Administradores<br><br>
+                    Módulo Auxiliares<br><br>
+                    Módulo Productos<br><br>
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    12. <br><br>
+                    12.1<br><br>
+                    12.2<br><br>
+                    12.3<br><br>
+
+                </td>
+                <td>
+                    <strong>Fase de Pruebas del Software</strong><br><br>
+                    TODO<br><br>
+                    TODO<br><br>
+                    TODO<br><br>
+                </td>
+
+            </tr>
+            <tr>
+                <td>13.</td>
+                <td>Conclusiones</td>
+            </tr>
+            <tr>
+                <td>14. </td>
+                <td>Referencias Bibliograficas</td>
+            </tr>
+
+        </tbody>
+
+    </table><br><br>
+
+    <h1>1. Introduccion</h1>
+    <h3>Proyecto desarrollado para almacenes de tipo <strong>Hipermercado en este caso Jumbo Cencosud superficies superiores a los 2500 metros cuadrados o Supermercados menos o igual a 2.500mts. </strong>En este tipo de establecimientos los consumidores pueden ir escogiendo los productos que desean, y los van incluyendo en sus carros de la compra. Al finalizar, hay un servicio de cajas donde pueden pagar la compra que se ha realizado.<br><br>Cuenta con la participacion de diferentes marcas, ademas de su amplio portafolio de marcas propias dando como resultado una gran cantidad de productos</h3><br><br>
+    <hr><br>
+    <h1>2. Planteamiento del problema</h1><br>
+    <h3>La necesidad que se pretende solucionar con el presente proyecto es minimizar los valores porcentuales en las mermas dentro de la cadena de recepción, almacenaje, distribución y posterior venta, especialmente en las mermas por vencimiento, a través de un sistema de alertas tempranas tipo semáforo.Siendo esta, una herramienta útil de control en los inventarios, minimizando las pérdidas que pueden llegar a afectar directamente la rentabilidad de la compañía.<br><br> 
+        El origen donde se pueden provocar el aumento de las mermas a lo largo de los procesos radica en lo siguiente:<br><br>
+
+        • No hay suficiente tiempo para hacer seguimiento continuo a la fecha de vencimiento en lo productos.<br>
+        • Alto volumen de los stocks que impiden un control unitario de los productos.<br>
+        • El llevar el registro manual de las fechas de vencimientos de los productos pueden llegar a no ser exacto debido a errores o fallas humanas no intencionadas.<br>
+        • Después del estudio realizado (entrevistas) se detectó que el nivel de porcentaje de merma por vencimiento es de 3,8.<br><br>
+
+
+        Con lo anteriormente planteado, se evidencia la urgente necesidad de diseñar un aplicativo web para el control de mermas en los inventarios en la empresa CENCOSUD, con un software muy funcional, el cual contribuya al mejoramiento de los procesos. El desarrollo de dicho sistema generaría fidelidad y satisfacción en los clientes al saber que están comprando productos de calidad y no vencidos.
+    </h3><br><br><hr><br>
+    <h1>3. Justificacion del Proyecto</h1>
+    <br><h3>La problemática de la organización consiste en que no cuentan con un sistema adecuado para llevar el control de mermas por vencimiento desde el ingreso y salidas de mercancías de bodega hasta las salas de ventas, ya que este procedimiento se hace de forma manual los que origina varios inconvenientes como: <br><br>•	No se tienen datos actualizados de la existencia de productos próximos a vencer en bodega como en la sala de ventas.<br>• No se puede hacer seguimiento continuo aquellos productos que tienen fecha de vencimiento de manera oportuna ocasionando pérdidas económicas para el Supermercado.<br>•	No se genera reportes oportunos a partir del ingreso de la mercancía al área de recibo lo cual no hay una trazabilidad de la fecha de vencimiento.<br><br>Por lo anterior existe la necesidad de desarrollar un software (Sistema de información) que genere alertar tempranas tipo semáforo que permita controlar las mermas por vencimiento, nace de la necesidad de tener un mayor control de estas dentro de la cadena de recepción, almacenamiento, distribución y posterior venta.<br><br>Ventajas de Desarrollar un software de alertas tipo semáforo para el control de inventarios en la empresa CENCOSUD <br><br>• Permite un mayor control de las mermas mediante la retroalimentación del sistema.<br>• Genera un mayor volumen de ventas a partir de la liquidación de los productos próximos a vencer, a través de grandes ofertas y descuentos.<br>•  Representa mayor satisfacción y fidelidad en el cliente, teniendo en cuenta los productos que va a adquirir son de calidad y no están vencidos ni cercanos a vencer. De esta manera aumentamos el nivel de satisfaccion del cliente respecto a la calidad del producto.<br>• Reduce costos y tiempos en el almacén, al disminuir el porcentaje de perdida de los productos en merma por vencimiento. </h3><br><br><hr><br>
+    <h1>4. Objetivo General.</h1><br>
+    <h3>Diseñar y Desarrollar un sistema de alertas tempranas tipo semáforo para el control de las mermas por vencimiento en los inventarios de la empresa almacenes jumbo- Cencosud</h3><br>
+    <h1>5. Objetivos Especificos</h1>
+    <h3>•	Analizar y cuantificar la información recolectada (entrevistas, encuestas, informe generado).<br>•	Diseñar y construir una base de datos con la información requerida para controlar los vencimientos en los productos.<br>•	Realizar las pruebas pertinentes para su desarrollo y su correcto funcionamiento. <br>•	Diseñar el modelo de un software que genere alertas para el control de mermas.<br>•	Desarrollar un sistema único de alerta que sea visible para cada uno de los participantes del proceso.</h3><br><br><hr><br>
+    <h1>6. Marco Teorico</h1><br>
+    <table>
+        <thead>
+            <th>Concepto</th>
+            <th>Definicion</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    Merma
+                </td>
+                <td>
+                La merma es la pérdida de valor de existencias consistente en la diferencia entre el stock de las mismas que aparece reflejado en la contabilidad y las existencias reales que hay en el almacén de la compañía.  
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Stock
+                </td>
+                <td>
+                Refiere a la cantidad de bienes o productos que dispone una organización.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Modulo
+                </td>
+                <td>
+                En programación, un módulo es un software que agrupa un conjunto de subprogramas y estructuras de datos. Los módulos son unidades que pueden ser compiladas por separado y los hace reusables, promueven el encapsulamiento pudiendo generar programas complejos de fácil comprención.
+                </td>
+            </tr>
+        </tbody>
+    </table><br><br><hr><br>
+
+    <h1>7. Tecnologias y Lenguajes de Programacion</h1>
+    <h3>PHP: Es un lenguaje de código abierto muy popular especialmente adecuado para el desarrollo web y que puede ser incrustado en HTML. Lo que distingue a PHP de algo del lado del cliente como Javascript es que el código es ejecutado en el servidor, generando HTML y enviándolo al cliente.<br><br>
+    HTML: es un lenguaje de marcado que se utiliza para el desarrollo de páginas de Internet. Se trata de la sigla que corresponde a HyperText Markup Language, es decir, Lenguaje de Marcas de Hipertexto, que podría ser traducido como Lenguaje de Formato de Documentos para Hipertexto.<br><br>
+    CSS:  El lenguaje CSS permite presentar, de manera estructurada, un documento que fue escrito en un lenguaje de marcado. Se usa especialmente en el diseño visual de un sitio web cuando las páginas se hallan escritas en XML o HTML.<br><br>
+    MySQL: Es el nombre de un sistema que permite la gestión de bases de datos. Se trata de la opción más utilizada para aplicaciones que se basan en la Web. </h3>
+    <br><br><hr><br>
+    <h1>8. Metodologías para el desarrollo de aplicaciones web:</h1>
+    <h3>UME Propuesta de ingeniería web basada en UML: UME es un método, de ingeniería WEB orientada a objetos basada en UML, que puede ser utilizado para la especificación de aplicaciones WEB.<br><br> </h3><hr><br>
+    <h1>9. Fases del Proyecto Aplicativo Web</h1>
+    <h2>Documentacion</h2>
+    <h2>9.1 Análisis de Requisitos del Software</h2>
+    <button ><a href="./src/ESPECIFICACION.pdf" target="_blank">Analisis de Requisistos</a></button><br>
+    <br>
+    <h2>9.2 Diseño del sistema</h2>
+    <button ><a href="./src/Diseño.pdf" target="_blank">Diseño del Sistema</a></button>
+    <br>
+    <h2>9.3 Codificación</h2>
+    <button> <a href="https://github.com/AndresGiraldo7/SenaProyect" target="_blank">Codificacion</a></button>
+    <br>
+    <h2>9.4 Pruebas y Error</h2>
+    <br>
+    <button> <a href="./src/Tabla.xlsx" target="_blank">Pruebas y Error</a></button>
+    <br>
+    <h2>9.5 Validacíon y Verificacíon del Producto Software</h2>
+    <br><hr><br>
+    <h1>10 Desarrollo de la Aplicación</h1>
+    <h2>10.1 Fase de Análisis</h2>
+    <h3>Diagramas</h3>
+    <br>
+    <button><a href="./src/CONCEPTUALversion2.0.pdf" target="_blank">Diagramas</a></button><br><br><br><hr>
+    <br>
+    <h1>11 Fase de Diseño y Desarrollo</h1>
+    <h2>11.1 Modulo de Usuarios</h2>
+    <br>
+    <button><a href="https://github.com/AndresGiraldo7/SenaProyect/tree/master/Usuarios" target="_blank">Ver Modulo de Usuarios</a></button>
+    <br>
+    <h2>11.2 Módulo de Administradores</h2>
+    <br>
+    <button><a href="https://github.com/AndresGiraldo7/SenaProyect/tree/master/Administradores" target="_blank">Ver Modulo de Usuarios</a></button>
+    <br>
+    <h2>11.3 Módulo de Auxiliares</h2>
+    <br>
+    <button><a href="https://github.com/AndresGiraldo7/SenaProyect/tree/master/Auxiliares" target="_blank">Ver Modulo de Usuarios</a></button>
+    <br>
+    <h2>11.4 Módulo de Productos</h2>
+    <br>
+    <button><a href="https://github.com/AndresGiraldo7/SenaProyect/tree/master/Productos" target="_blank">Ver Modulo de Usuarios</a></button>
+    <br><br><hr><br>
+    <h1>13. Conclusiones</h1><br>
+    <h3>1. La Separación clara del codigo  dónde tiene que ir cada tipo de lógica, facilitando el mantenimiento y la escalabilidad de la aplicación. <br><br> 2. Los módulos son más fáciles de entender, debido a que son subsistemas autocontenidos, para una o varias funciones específicas.<br><br> 3. Los componentes son facilmente reutilizables.</h3>
+    <br><hr><br>
+    <h1>14. Referencias Bibliograficas</h1>
+    <a href="https://definicion.de/mysql/" target="_blank">https://definicion.de/mysql/</a><br>
+    <a href="https://definicion.de/html/" target="_blank">https://definicion.de/html/</a><br>
+    <a href="https://definicion.de/css/" target="_blank">https://definicion.de/css/</a><br>
+    <a href="https://www.php.net/manual/es/intro-whatis.php" target="_blank">https://www.php.net/manual/es/intro-whatis.php</a><br>
+    <a href="https://definicion.de/stock/" target="_blank">https://definicion.de/stock/</a><br>
+    <a href="https://economipedia.com/definiciones/merma.html" target="_blank">https://economipedia.com/definiciones/merma.html</a><br>
+    <a href="https://sites.google.com/site/ingenieriawebuc/home/contenido/uwe" target="_blank">https://sites.google.com/site/ingenieriawebuc/home/contenido/uwe</a><br>
+    <a href="https://www.alegsa.com.ar/Dic/modulo.php" target="_blank">https://www.alegsa.com.ar/Dic/modulo.php</a><br>
+    </div>
+</body>
+<footer class="container">
+
+     
+      <br><br><br><br><br>
+      Designed by <a class="dev" href="https://andresggdeveloper.herokuapp.com/">Andrés Giraldo | Developer</a>
+      
+</footer>
+
+</html>
